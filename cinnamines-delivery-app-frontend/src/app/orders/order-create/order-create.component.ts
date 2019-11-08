acbb1808d5c0ff407eb81e27c2b93eca3f100475
Judyhouser80 <judyhouser80@gmail.com>
2019-11-07 21:20:17 -0500

    some cleanup

--- cinnamines-delivery-app-frontend/src/app/orders/order-create/order-create.component.ts
+++ cinnamines-delivery-app-frontend/src/app/orders/order-create/order-create.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class OrderCreateComponent implements OnInit {
+  newOrder = '';
+
+  onAddOrder() {
+    alert('Thank you for your order!');
+  }
 
   constructor() { }
 
