4314fb824d9be851fba57777420e76e61be866c9
Judyhouser80 <judyhouser80@gmail.com>
2019-11-12 18:27:13 -0500

    updates on authorization for nav bar and edit/delete buttons

--- cinnamines-delivery-app-frontend/src/app/header/header.component.ts
+++ cinnamines-delivery-app-frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { AuthService } from '../auth/auth.service';
 
@@ -8,10 +9,15 @@ import { AuthService } from '../auth/auth.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+  });
   }
 
   onLogout() {
--- cinnamines-delivery-app-frontend/src/app/orders/order-history/order-history.component.ts
+++ cinnamines-delivery-app-frontend/src/app/orders/order-history/order-history.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Order } from 'src/app/models/order';
 import { OrdersService } from 'src/services/orders.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-order-history',
@@ -10,11 +11,15 @@ import { OrdersService } from 'src/services/orders.service';
 })
 export class OrderHistoryComponent implements OnInit, OnDestroy {
   orders: Order[] = [];
+  userIsAuthenticated = false;
   private ordersSub: Subscription;
+  private authStatusSub: Subscription;
 
-  constructor(public ordersService: OrdersService) { }
+  constructor(public ordersService: OrdersService, private authService: AuthService) { }
 
   ngOnInit() {
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {this.userIsAuthenticated = isAuthenticated;
+    });
     // this.ordersService.getOrders();
     // this.ordersSub = this.ordersService.getOrderUpdateListener()
     //   .subscribe((orders: Order[]) => {
